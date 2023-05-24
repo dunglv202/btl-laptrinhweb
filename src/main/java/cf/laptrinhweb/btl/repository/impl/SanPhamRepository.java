2c5a600ff9ce1c6792f4a76048561c7388651f1c
huybq-02 <108663038+huybq-02@users.noreply.github.com>
2023-05-24 22:36:55 +0700

    tao SanPhamRepository.java

---
+++ src/main/java/cf/laptrinhweb/btl/repository/impl/SanPhamRepository.java
@@ -0,0 +1,9 @@
+package cf.laptrinhweb.btl.repository;
+
+import cf.laptrinhweb.btl.model.SanPham;
+
+import java.util.Optional;
+
+public interface SanPhamRepository extends JdbcRepository<SanPham> {
+   
+}
