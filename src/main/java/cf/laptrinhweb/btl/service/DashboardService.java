a64a28c12dc7d27d11743a348c6a08543f05b35a
huybq-02 <108663038+huybq-02@users.noreply.github.com>
2023-06-12 00:45:30 +0700

    them ser

--- src/main/java/cf/laptrinhweb/btl/service/DashboardService.java
+++ src/main/java/cf/laptrinhweb/btl/service/DashboardService.java
@@ -1,6 +1,7 @@
 package cf.laptrinhweb.btl.service;
 
 import cf.laptrinhweb.btl.model.BanGhiDuLieu;
+import cf.laptrinhweb.btl.model.SanPhamMuaNhieu;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -9,4 +10,5 @@ import java.util.Map;
 public interface DashboardService {
     Map<String, Object> layThongKeDoanhThu(LocalDate ngayBatDau, LocalDate ngayKetThuc);
     double tinhTiLeHuyDon(LocalDate ngayBatDau, LocalDate ngayKetThuc);
+     List<SanPhamMuaNhieu> lietKe();
 }
