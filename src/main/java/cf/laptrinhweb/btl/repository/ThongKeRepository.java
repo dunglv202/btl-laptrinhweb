8717a1bec9e7fca9d37dfd4a415213faa9bc316f
huybq-02 <108663038+huybq-02@users.noreply.github.com>
2023-06-13 00:07:22 +0700

    them

--- src/main/java/cf/laptrinhweb/btl/repository/ThongKeRepository.java
+++ src/main/java/cf/laptrinhweb/btl/repository/ThongKeRepository.java
@@ -1,11 +1,20 @@
 package cf.laptrinhweb.btl.repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Map;
+
+import cf.laptrinhweb.btl.model.KhachHangMuaNhieu;
+import cf.laptrinhweb.btl.model.SanPhamMuaNhieu;
+import cf.laptrinhweb.btl.model.TheLoaiMuaNhieu;
+import cf.laptrinhweb.btl.model.ThuongHieuMuaNhieu;
 
 public interface ThongKeRepository extends JdbcRepository {
     Map<LocalDate, Double> thongKeDoanhThu(LocalDate ngayBatDau, LocalDate ngayKetThuc);
     double layGiaTriDonTrungBinh(LocalDate ngayBatDau, LocalDate ngayKetThuc);
     double tinhTiLeHuyDon(LocalDate ngayBatDau, LocalDate ngayKetThuc);
-     List<SanPhamMuaNhieu> lietKe();
+    List<SanPhamMuaNhieu> lietKe();
+    List<KhachHangMuaNhieu> lietKe2();
+    List<TheLoaiMuaNhieu> lietKe3();
+    List<ThuongHieuMuaNhieu> lietKe4();
 }
