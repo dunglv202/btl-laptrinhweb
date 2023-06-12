2b7e520163172f5963cd204a3b08b93ec0e5614c
huybq-02 <108663038+huybq-02@users.noreply.github.com>
2023-06-13 00:08:20 +0700

    them

--- src/main/java/cf/laptrinhweb/btl/repository/impl/ThongKeRepositoryImpl.java
+++ src/main/java/cf/laptrinhweb/btl/repository/impl/ThongKeRepositoryImpl.java
@@ -1,15 +1,20 @@
 package cf.laptrinhweb.btl.repository.impl;
 
 import cf.laptrinhweb.btl.constant.TinhTrangDon;
-import cf.laptrinhweb.btl.repository.ThongKeRepository;
+import cf.laptrinhweb.btl.model.KhachHangMuaNhieu;
 import cf.laptrinhweb.btl.model.SanPhamMuaNhieu;
+import cf.laptrinhweb.btl.model.TheLoaiMuaNhieu;
+import cf.laptrinhweb.btl.model.ThuongHieuMuaNhieu;
+import cf.laptrinhweb.btl.repository.ThongKeRepository;
 
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ThongKeRepositoryImpl implements ThongKeRepository {
@@ -100,7 +105,8 @@ public class ThongKeRepositoryImpl implements ThongKeRepository {
             throw new RuntimeException("Khong the tinh toan ti le huy don", e);
         }
     }
-        @Override
+    
+    @Override
     public List<SanPhamMuaNhieu> lietKe() {
         try (Connection ketNoi = moKetNoi()) {
             PreparedStatement ps = ketNoi.prepareStatement("""
@@ -126,5 +132,90 @@ public class ThongKeRepositoryImpl implements ThongKeRepository {
         } catch (Exception e) {
             throw new RuntimeException("San pham moi cap nhat", e);
         }
-    }                                  
+    }
+    @Override
+    public List<KhachHangMuaNhieu> lietKe2(){
+    	  try (Connection ketNoi = moKetNoi()) {
+              PreparedStatement ps = ketNoi.prepareStatement("""
+                SELECT n.ten_dang_nhap,n.ten_hien_thi,sum(d.tong_tien) as tt FROM dat_hang d 
+              		JOIN nguoi_dung n ON d.ma_nguoi_dat = n.ma_nguoi_dung
+              		GROUP BY  d.ma_nguoi_dat
+              		ORDER BY tt DESC
+              		LIMIT 3;
+              """);
+              ResultSet resultSet = ps.executeQuery();
+              List<KhachHangMuaNhieu> dsKH = new ArrayList<>();
+              int dem = 1;
+              while (resultSet.next()) {
+                  KhachHangMuaNhieu kh = new KhachHangMuaNhieu();
+                  kh.setStt(dem++);
+                  kh.setTenDangNhap(resultSet.getString("ten_dang_nhap"));
+                  kh.setTenHienThi(resultSet.getString("ten_hien_thi"));
+                  kh.setTongTien(resultSet.getDouble("tt"));
+                  dsKH.add(kh);
+             
+              }
+              return dsKH;
+          } catch (Exception e) {
+              throw new RuntimeException("Khach hang moi", e);
+          }
+    }
+    
+    @Override
+    public List<TheLoaiMuaNhieu> lietKe3(){
+    	  try (Connection ketNoi = moKetNoi()) {
+              PreparedStatement ps = ketNoi.prepareStatement("""
+              SELECT tl.ten_the_loai, SUM(spd.so_luong) AS tong_so_luong
+              		FROM btl_ltw.the_loai tl
+              		INNER JOIN san_pham sp ON tl.ma_the_loai = sp.ma_the_loai
+              		INNER JOIN san_pham_dat spd ON sp.ma_san_pham = spd.ma_san_pham
+              		GROUP BY tl.ten_the_loai
+              		ORDER BY tong_so_luong DESC
+              		LIMIT 3;
+              """);
+              ResultSet resultSet = ps.executeQuery();
+              List<TheLoaiMuaNhieu> dsTL = new ArrayList<>();
+              int dem = 1;
+              while (resultSet.next()) {
+                  TheLoaiMuaNhieu tl = new TheLoaiMuaNhieu();
+                  tl.setStt(dem++);
+                  tl.setTenTheLoai(resultSet.getString("ten_the_loai"));
+                  tl.setSoLuong(resultSet.getInt("tong_so_luong"));
+                  dsTL.add(tl);
+             
+              }
+              return dsTL;
+          } catch (Exception e) {
+              throw new RuntimeException("The loai moi", e);
+          }
+    }
+    
+    @Override
+    public List<ThuongHieuMuaNhieu> lietKe4(){
+    	  try (Connection ketNoi = moKetNoi()) {
+              PreparedStatement ps = ketNoi.prepareStatement("""
+              		SELECT th.ten_thuong_hieu, SUM(spd.so_luong) AS tong_so_luong
+              		FROM thuong_hieu th
+              		INNER JOIN san_pham sp ON th.ma_thuong_hieu = sp.ma_thuong_hieu
+              		INNER JOIN san_pham_dat spd ON sp.ma_san_pham = spd.ma_san_pham
+              		GROUP BY th.ten_thuong_hieu
+              		ORDER BY tong_so_luong DESC
+              		LIMIT 3;
+              """);
+              ResultSet resultSet = ps.executeQuery();
+              List<ThuongHieuMuaNhieu> dsTH = new ArrayList<>();
+              int dem = 1;
+              while (resultSet.next()) {
+                  ThuongHieuMuaNhieu th = new ThuongHieuMuaNhieu();
+                  th.setStt(dem++);
+                  th.setTenThuongHieu(resultSet.getString("ten_thuong_hieu"));
+                  th.setSoLuong(resultSet.getInt("tong_so_luong"));
+                  dsTH.add(th);
+             
+              }
+              return dsTH;
+          } catch (Exception e) {
+              throw new RuntimeException("Thuong hieu moi", e);
+          }
+    }
 }
