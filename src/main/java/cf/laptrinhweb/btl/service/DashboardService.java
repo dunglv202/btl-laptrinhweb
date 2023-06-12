05f5a4e31253586dac2d0f3365cd69fe3b8fb4c0
huybq-02 <108663038+huybq-02@users.noreply.github.com>
2023-06-13 00:10:32 +0700

    thêm

--- src/main/java/cf/laptrinhweb/btl/service/DashboardService.java
+++ src/main/java/cf/laptrinhweb/btl/service/DashboardService.java
@@ -1,7 +1,10 @@
 package cf.laptrinhweb.btl.service;
 
 import cf.laptrinhweb.btl.model.BanGhiDuLieu;
+import cf.laptrinhweb.btl.model.KhachHangMuaNhieu;
 import cf.laptrinhweb.btl.model.SanPhamMuaNhieu;
+import cf.laptrinhweb.btl.model.TheLoaiMuaNhieu;
+import cf.laptrinhweb.btl.model.ThuongHieuMuaNhieu;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -10,5 +13,8 @@ import java.util.Map;
 public interface DashboardService {
     Map<String, Object> layThongKeDoanhThu(LocalDate ngayBatDau, LocalDate ngayKetThuc);
     double tinhTiLeHuyDon(LocalDate ngayBatDau, LocalDate ngayKetThuc);
-     List<SanPhamMuaNhieu> lietKe();
+    List<SanPhamMuaNhieu> lietKe();
+    List<KhachHangMuaNhieu> lietKe2();
+    List<TheLoaiMuaNhieu> lietKe3();
+    List<ThuongHieuMuaNhieu> lietKe4();
 }
