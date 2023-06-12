594ddfdf5cbcd648d419ad63a52821691de69a3d
huybq-02 <108663038+huybq-02@users.noreply.github.com>
2023-06-13 00:05:15 +0700

    Add files via upload

---
+++ src/main/java/cf/laptrinhweb/btl/model/KhachHangMuaNhieu.java
@@ -0,0 +1,14 @@
+package cf.laptrinhweb.btl.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class KhachHangMuaNhieu {
+	private int stt;
+	private String tenDangNhap;
+	private String tenHienThi;
+	private double tongTien;
+}
