921d1b459e713d460f85a35b9bb2370871467986
Nguyenvanhieu229 <hieunguyenduyenhai22092002@gmail.com>
2023-06-13 13:46:31 +0700

    cap nhat trang thai don

---
+++ src/main/java/cf/laptrinhweb/btl/controller/donhang/CapNhatTrangThaiDon.java
@@ -0,0 +1,29 @@
+package cf.laptrinhweb.btl.controller.donhang;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import cf.laptrinhweb.btl.constant.TrangThaiDon;
+import cf.laptrinhweb.btl.repository.DatHangRepository;
+import cf.laptrinhweb.btl.repository.impl.DatHangRepositoryImpl;
+
+/**
+ * Servlet implementation class CapNhatTrangThaiDon
+ */
+@WebServlet("/don-hang/cap-nhat")
+public class CapNhatTrangThaiDon extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private DatHangRepository datHangRepository = new DatHangRepositoryImpl();
+       
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		TrangThaiDon moi = TrangThaiDon.cuaGiaTri(Integer.valueOf(request.getParameter("trangThai")));
+		Long maDH = Long.parseLong(request.getParameter("maDatHang"));
+		datHangRepository.CapNhatTrangThaiDon(maDH, moi);
+		response.sendRedirect(request.getHeader("Referer"));
+	}
+
+}
