74d2ded137fa53ad813bdef78ee30efd8a58afb7
dunglv <dunglv@bsscommerce.com>
2024-03-12 20:22:44 +0700

    update database info

--- src/main/java/cf/laptrinhweb/btl/constant/CSDL.java
+++ src/main/java/cf/laptrinhweb/btl/constant/CSDL.java
@@ -2,7 +2,7 @@ package cf.laptrinhweb.btl.constant;
 
 public class CSDL {
     public static final String DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
-    public static final String DIA_CHI = "jdbc:mysql://btl-ltw.cgg1solrwn4m.ap-southeast-1.rds.amazonaws.com:3306/btl_ltw";
-    public static final String TEN_DANG_NHAP = "admin";
-    public static final String MAT_KHAU = "FU5O335PdjuKZqgS";
+    public static final String DIA_CHI = "jdbc:mysql://viaduct.proxy.rlwy.net:10273/railway?createDatabaseIfNotExist=true";
+    public static final String TEN_DANG_NHAP = "root";
+    public static final String MAT_KHAU = "VXvckNEiZsckeXmmgrRjpRTEaneksDtU";
 }
